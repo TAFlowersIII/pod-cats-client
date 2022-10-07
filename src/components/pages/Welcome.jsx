9214618660c402b0b4aa90ad0a1bd2ce4830757a
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-10-07 17:50:12 -0400

    changed about.jsx, change sizing of image at welcome.jsx

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -7,7 +7,11 @@ export default function Welcome() {
 			<h1>Welcome To Pod Cats!</h1>
 			<h3>Browse some cats, and save your favorites!</h3>
 		<br></br>
-			<img src="https://coed.com/wp-content/uploads/2018/01/shutterstock_1031589889.jpg" alt="a picture of a smiling kitten"></img>
+			<img 
+			src="https://coed.com/wp-content/uploads/2018/01/shutterstock_1031589889.jpg"
+			id="welcome_photo"
+			alt="a picture of a smiling kitten"
+			></img>
 		<br></br>
 			<p>“Owners of dogs will have noticed that, if you provide them with food and water and shelter and affection, they will think you are God. Whereas owners of cats are compelled to realize that, if you provide them with food and water and affection, they draw the conclusion that they are God.” – Christopher Hitchens</p>
 
