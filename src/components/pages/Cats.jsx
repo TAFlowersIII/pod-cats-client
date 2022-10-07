9e15935fe263fde0f749edf954cfb31492b4d781
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-07 13:55:01 -0700

    add Cats.jsx and add imports to App

--- src/components/pages/Cats.jsx
+++ src/components/pages/Cats.jsx
@@ -6,19 +6,28 @@ export default function Cats(){
     const [cats, setCats] = useState([])
 
     useEffect(() =>{
-        const kitties = async () => {
+        const getCats = async () => {
             try{
-                const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10
-                `)
+                const response = await axios.get(`https://api.thecatapi.com/v1/images/search?limit=10`)
                 console.log(response.data)
+                setCats(response.data)
             }catch(err){
                 console.log(err)
             }
         }
+        getCats()
     },[])
+
+    const showCats = cats.forEach(cat => {
+        return(
+            <div id={cat.id}>
+                <img src={cat.url} alt="a very cute cat"/>
+            </div>
+        )
+    })
     return(
         <div>
-
+            {showCats}
         </div>
     )
 }
