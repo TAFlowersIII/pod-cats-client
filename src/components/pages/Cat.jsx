8448b21827e55534fa9e1fda3328dbdb4d2e02d0
KhakiSuitValentino <iamschoolcraft@gmail.com>
2022-10-10 12:52:46 -0400

    changes to cat.jsx: JWT user data sent to back end

--- src/components/pages/Cat.jsx
+++ src/components/pages/Cat.jsx
@@ -2,8 +2,10 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { dblClick } from '@testing-library/user-event/dist/click'
+import jwt_decode from 'jwt-decode'
 
-export default function Cat() {
+
+export default function Cat({currentUser}) {
     const [cat, setCat] = useState({})
     const [errorMessage, setErrorMessage] = useState('')
 
@@ -14,7 +16,15 @@ export default function Cat() {
         const getCat = async () => {
             try {
                 const response = await axios.get(`https://api.thecatapi.com/v1/images/${id}`)
-                // console.log(response.data)
+                const decodedoken = jwt_decode(localStorage.getItem('jwt'))
+
+
+                //Passed in the userid from the decoded token
+                response.data["userId"] = decodedoken.id;
+                // console.log('response.data:', response.data);
+                // console.log('decodeoken.id', decodedoken.id);
+
+
                 setCat(response.data)        
             } catch(err) {
                 console.warn(err)
@@ -34,7 +44,7 @@ export default function Cat() {
             e.preventDefault()
             // need to determine where this is posting
             await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/cats/new`, cat)
-            console.log()
+            console.log("the current user is: " + currentUser)
         } catch(err) {
             console.warn(err)
             if (err.response) {
