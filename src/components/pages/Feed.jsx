9fbd46abf92ee780e1ac47bc11a9438172f59a53
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-10 10:41:22 -0700

    adds feed to navbar and feed functionality

--- src/components/pages/Feed.jsx
+++ src/components/pages/Feed.jsx
@@ -1,7 +1,43 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 export default function Feed() {
+
+    const [msg, setMsg] = useState('')
+	const [cats, setCats] = useState([])
+
+    useEffect(() => {
+        const getCats = async () => {
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/cats`)
+                setCats(response.data)
+            }catch(err){
+                console.warn(err)
+                if(err.response){
+                    if(err.response){
+                        setMsg(err.response.data.message)
+                    }
+                }
+            }
+        }
+        getCats()
+    },[])
+    
+    const showCats = cats.map(cat => {
+        const catComment = cat.comments.map(comment =>{
+            <p>{comment}</p>
+        })
+        return(
+        <div key={cat._id}>
+            <img src={cat.img_Url} />
+            <p>{catComment}</p>
+        </div>
+        )
+    })
+
     return (
         <div>
-            Feed here 👏
+            {showCats}
         </div>
     )
 }
