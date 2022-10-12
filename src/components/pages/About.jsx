027f47d8ec66601c164f2ebe4cff080e477e76f1
syorn96 <syorn2014@gmail.com>
2022-10-12 12:08:44 -0700

    updates about.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,24 +4,46 @@ import { Link } from 'react-router'
 export default function About() {
      return (
           <div>
-               <h1>About Pod Cats:</h1>
+               <h1>About the Pod Cats Team</h1>
                <h3>In software engineering, group projects are considered especially challenging.</h3>
                <h3>At General Assembly, the dedicated web developers who love their pet cats have created this website known as Pod Cats.</h3>
-               <h3>These are their stories.</h3>
+               <h3>These are their stories...</h3>
 
                <br></br>
                <br></br>
                <br></br>
 
                {/* Thomas: */}
-               <div>
-                    <img src='https://cdn2.thecatapi.com/images/16h.jpg'></img>
-                    <br></br>
-                    <h1>Thomas</h1>
-                    <br></br>
-                    <h4>Thomas is a grumpy old man on the outside, but a lil cutie patootie on the inside. He enjoys weird and obscure music, long bike rides through nature, deep talks about philosophy/the state of humanity, and immature humor- although he pretends to enjoy immature humor ironically. He is too hip and cool for his own damn good.</h4>
+               <div style={{display:'flex'}}>
+                    <div>
+                         <img src='https://cdn2.thecatapi.com/images/16h.jpg'></img>
+                         <br></br>
+                         <h1>Thomas</h1>
+                         <br></br>
+                         <h4>Thomas is a grumpy old man on the outside, but a lil cutie patootie on the inside. He enjoys weird and obscure music, long bike rides through nature, deep talks about philosophy/the state of humanity, and immature humor- although he pretends to enjoy immature humor ironically. He is too hip and cool for his own damn good.</h4>
+                    </div>
+                    <div>
+                         <img src='https://i.imgur.com/m6lOLAB.jpg'></img>
+                         <br></br>
+                         <h1>Stephen</h1>
+                         <br></br>
+                         <h4>Stephen enjoys meditating in his sleep. He ponders on the mysteries of life and enjoys learning new things. One thing to know about Stephen is that he truly enjoys making people laugh and helping others to the best of his abilities. His goal is to make people love cats just as much as he does.</h4>
+                    </div>
+                    <div>
+                         <img src='https://i.imgur.com/m6lOLAB.jpg'></img>
+                         <br></br>
+                         <h1>Matt</h1>
+                         <br></br>
+                         <h4>Thomas is a grumpy old man on the outside, but a lil cutie patootie on the inside. He enjoys weird and obscure music, long bike rides through nature, deep talks about philosophy/the state of humanity, and immature humor- although he pretends to enjoy immature humor ironically. He is too hip and cool for his own damn good.</h4>
+                    </div>
+                    <div>
+                         <img src='https://i.imgur.com/m6lOLAB.jpg'></img>
+                         <br></br>
+                         <h1>Devin</h1>
+                         <br></br>
+                         <h4>Thomas is a grumpy old man on the outside, but a lil cutie patootie on the inside. He enjoys weird and obscure music, long bike rides through nature, deep talks about philosophy/the state of humanity, and immature humor- although he pretends to enjoy immature humor ironically. He is too hip and cool for his own damn good.</h4>
+                    </div>
                </div>
-               
           </div>
      )
 }
