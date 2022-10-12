078b67b28bee7550c81b0710fd223fa23b4e0143
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-12 10:59:40 -0700

    forms updated, not centered

--- src/components/pages/NewCat.jsx
+++ src/components/pages/NewCat.jsx
@@ -57,36 +57,60 @@ export default function NewCat(){
             <h1>Add a new Cat</h1>
             <p>{errorMessage}</p>
 
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='header'>Your cat's name:</label>
-                    <input 
-                    type='text' 
-                    id='header' 
-                    value={cat.header}
-                    placeholder="Mister Snuggles"
-                    onChange={e => setCat({...cat, header: e.target.value})}></input>
-                </div>
-                <div>
-                    <label htmlFor='img_Url'>Image URL:</label>
-                    <input 
-                    type='text' 
-                    id='img_Url' 
-                    value={cat.img_Url}
-                    placeholder='Add a valid image URL' 
-                    onChange={e => setCat({...cat, img_Url: e.target.value})}></input>
-                </div>
-                <div>
-                    <label htmlFor='content'>Description:</label>
-                    <input 
-                    type='text' 
-                    id='content' 
-                    value={cat.content}
-                    placeholder='Describe your cat...' 
-                    onChange={e => setCat({...cat, content: e.target.value})}></input>
-                </div>
-                <button type='submit'>Add to Collection</button>
-            </form>
+            <div className="w-full max-w-xs object-center">
+                <form 
+                    className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+                    onSubmit={handleSubmit}
+                >
+                    <div className='mb-4'>
+                        <label 
+                            className="block text-gray-700 text-sm font-bold mb-2" 
+                            htmlFor='header'
+                        >
+                            Your cat's name:
+                        </label>
+                        <input 
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            type='text' 
+                            id='header' 
+                            value={cat.header}
+                            placeholder="Mister Snuggles"
+                            onChange={e => setCat({...cat, header: e.target.value})}></input>
+                    </div>
+
+                    <div className='mb-4'>
+                        <label 
+                            className="block text-gray-700 text-sm font-bold mb-2" 
+                            htmlFor='img_Url'
+                        >
+                            Image URL:
+                        </label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type='text' 
+                        id='img_Url' 
+                        value={cat.img_Url}
+                        placeholder='Add a valid image URL' 
+                        onChange={e => setCat({...cat, img_Url: e.target.value})}></input>
+                    </div>
+                    <div className='mb-4'>
+                        <label 
+                            className="block text-gray-700 text-sm font-bold mb-2"
+                            htmlFor='content'
+                        >
+                            Description:
+                        </label>
+                        <input 
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type='text' 
+                        id='content' 
+                        value={cat.content}
+                        placeholder='Describe your cat...' 
+                        onChange={e => setCat({...cat, content: e.target.value})}></input>
+                    </div>
+                    <button type='submit' className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Add to Collection</button>
+                </form>
+            </div>
         </div>
     )
 }
