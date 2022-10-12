ecf25aeca2cea25fc67cba671148fbf73273f9bf
devin-lynch <devin.l.lynch@gmail.com>
2022-10-12 12:24:27 -0500

    Fix spacing

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -99,8 +99,10 @@ export default function Profile({ currentUser, setCurrentUser, handleLogout }) {
 			<img className='rounded-3xl' src={cat.img_Url} alt="a cute kitty"/>
 			<p>{cat.content}</p>
 			{/* <p>{catComment}</p> */}
-			<br/>
+			{/* <br/> */}
 			<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" id={cat._id} onClick={() => handleDelete(`${cat._id}`)}>Delete from Favorites</button>
+			<br></br>
+			<br></br>
 		</div>
 		)
 	})
