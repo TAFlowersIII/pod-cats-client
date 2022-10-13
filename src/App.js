9de6b74ee594d5feb506b780f31e45eb2d4c08f5
devin-lynch <devin.l.lynch@gmail.com>
2022-10-12 23:17:31 -0500

    Fix mobile view and style vh fixed images

--- src/App.js
+++ src/App.js
@@ -108,12 +108,12 @@ function App() {
           <Route
           path='/profile/account' element = {<Account currentUser={currentUser} handleLogout={handleLogout} setCurrentUser={setCurrentUser}/>}/>
         </Routes>
-        <img className='leftCat' src= 'https://i.imgur.com/fGAzAtW.png' />
-        <img className='rightCat' src= 'https://i.imgur.com/Tpl6G6b.png' />
-        <img className='rightLongCat' src='https://i.imgur.com/nvUPSwj.png' />
-        <img className='leftLongCat' src='https://i.imgur.com/PWQqz04.png' />
-        <img className='leftTopCat' src='https://i.imgur.com/UYWkQb9.png' />
-        <img className='rightTopCat' src='https://i.imgur.com/IWZXtw8.png' />        
+        <img className='sideCat leftCat' src= 'https://i.imgur.com/fGAzAtW.png' />
+        <img className='sideCat rightCat' src= 'https://i.imgur.com/Tpl6G6b.png' />
+        <img className='sideCat rightLongCat' src='https://i.imgur.com/nvUPSwj.png' />
+        <img className='sideCat leftLongCat' src='https://i.imgur.com/PWQqz04.png' />
+        <img className='sideCat leftTopCat' src='https://i.imgur.com/UYWkQb9.png' />
+        <img className='sideCat rightTopCat' src='https://i.imgur.com/IWZXtw8.png' />        
       </div>
     </Router>
   );
